f43c2c6d0e0cc87a3c584cb589f81c66751818fe
Leonidas <lossas85@gmail.com>
2013-07-17 18:28:56 +0200

    add init.pp

---
+++ manifest/init.pp
@@ -0,0 +1,17 @@
+class FileCreation{
+
+    file {'/tmp/test1':
+      ensure  => present,
+      content => "Hi.",
+    }
+
+    file {'/tmp/test2':
+      ensure => directory,
+      mode   => 644,
+    }
+
+    file {'/tmp/test3':
+      ensure => link,
+      target => '/tmp/test1',
+    }
+}
